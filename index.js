0fc93ec155a4ed5a6cc37dd9c43ff6467834bd3f
Scott Fredericksen <scott.fredericksen@gmail.com>
2018-12-17 17:37:14 -0700

    Hello Scott

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 app.use(bodyParser.json({ strict: false }));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello Scott!')
 })
 
 // Get User endpoint
